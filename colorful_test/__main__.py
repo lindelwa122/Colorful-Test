61a9cf9c0ac0968b49d449f1a973d67d7d11dc60
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-01-27 12:16:14 +0200

    Fix bugs on the __main__ file.

--- colorful_test/__main__.py
+++ colorful_test/__main__.py
@@ -14,26 +14,34 @@ def main() -> None:
 
     args = argv[1:]
 
-    if len(args) < 2:
+    if len(args) < 1:
         print('Usage: python -m colorful-test <directory_name>/<file_name>')
+
+    run_tests(args)
 
 
 def execute_file(filename: str) -> None:
     """Uses subprocess to run a test file"""
-    subprocess.run(['python3', filename], capture_output=True, text=True)
+    result = subprocess.run(['python3', filename], capture_output=True, text=True)
+   
+    if result.stdout: 
+        print(result.stdout)
+    elif result.stderr:
+        print(result.stderr)
 
 
-def run_tests(args: List[str]) -> None:
+def run_tests(args: List[str], path='') -> None:
     """
     Takes a list of tests and runs them.
     """
-
+    
     for arg in args:
         if os.path.isdir(arg):
-            run_tests(os.listdir(arg))
+            new_path = path + arg
+            run_tests(os.listdir(arg), new_path)
 
         elif arg.startswith('test') and arg.endswith('.py'):
-            execute_file(arg)
+            execute_file(path + arg)
 
 
 if __name__ == '__main__':
